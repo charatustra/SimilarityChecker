7b9abe8509596333c7f8365bfe2828a9aa462311
jwoong23.seo <jwoong23.seo@samsung.com>
2024-06-13 16:09:52 +0900

    [refactoring] 같은 글자수 부분 점수 1

--- checker.py
+++ checker.py
@@ -1,15 +1,29 @@
+from checker_input import CheckerInput
+
 MIN_LENGTH_SCORE = 0
 MAX_LENGTH_SCORE = 60
 
 
 class Checker:
     def check(self, left: str, right: str):
-        if len(left) == len(right):
+        left_input = CheckerInput(left)
+        right_input = CheckerInput(right)
+        if left_input.length == right_input.length:
             return MAX_LENGTH_SCORE
         else:
-            long_str = left if len(left) >= len(right) else right
-            short_str = left if long_str != left else right
-            if len(long_str) >= len(short_str) * 2:
+            long_str, short_str = self.make_long_short_input(left_input, right_input)
+            if self.assert_min_length_score(long_str, short_str):
                 return MIN_LENGTH_SCORE
-            gap = len(long_str) - len(short_str)
-            return (len(short_str) - gap) / len(short_str) * MAX_LENGTH_SCORE
+            return self.calculate_partial_score(long_str, short_str)
+
+    def calculate_partial_score(self, long_str, short_str):
+        return ((short_str.length - (long_str.length - short_str.length)) / short_str.length
+                * MAX_LENGTH_SCORE)
+
+    def assert_min_length_score(self, long_str, short_str):
+        return long_str.length >= short_str.length * 2
+
+    def make_long_short_input(self, left_input, right_input):
+        long_str = left_input if left_input.length >= right_input.length else right_input
+        short_str = left_input if long_str != left_input else right_input
+        return long_str, short_str
